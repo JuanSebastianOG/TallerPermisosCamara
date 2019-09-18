49083567e974eed536f7ffd2ef15f9eb70aad6f8
Juan Sebastián OG <seposorio@hotmail.com>
2019-09-18 12:28:24 -0500

    Home Page
    Two buttons

--- app/src/main/java/com/example/sistemas/tallerpermisoscamara/MainActivity.java
+++ app/src/main/java/com/example/sistemas/tallerpermisoscamara/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.sistemas.tallerpermisoscamara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_camara;
+    Button btn_contactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         btn_camara=(Button) findViewById(R.id.btn_camara);
+        btn_contactos=(Button) findViewById(R.id.btn_contactos);
+
+
+        btn_camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_contactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
